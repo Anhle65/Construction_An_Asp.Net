9b4258d367e1e19ea835b3e83402b39f3f86b666
Anhle65 <127622946+Anhle65@users.noreply.github.com>
2024-01-29 16:15:51 +1300

    Add files via upload
    Create an endpoint which return a string

---
+++ UnderhoodASPMVC/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.AspNetCore.Builder;
+
+var greeting = new Greeting().Message("Vanh");
+Console.WriteLine($"Hello, World!");
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.MapGet("/", () => "It works");
+app.MapGet("/Vanh", () => greeting);
+app.Run();
