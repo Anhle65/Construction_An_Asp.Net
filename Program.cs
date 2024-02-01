bdce5b3f791f07de464e5a6305ff6da7e134d1ce
Anhle65 <127622946+Anhle65@users.noreply.github.com>
2024-02-02 11:30:05 +1300

    Add files via upload
    Creat a host running on a specific url before building a web app

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+var greeting = new Greeting().Message("Vanh");
+
+// Creat a host which run on a specific URL before a web application is created
+using (
+    var host = WebHost.Start("http://localhost:5001",router => router
+    .MapGet("hello/{name}", (req, res, data) =>
+        res.WriteAsync($"Hello, {data.Values["name"]}!"))
+    .MapGet("buenosdias/{name}", (req, res, data) =>
+        res.WriteAsync($"Buenos dias, {data.Values["name"]}!"))
+    .MapGet("throw/{message?}", (req, res, data) =>
+        res.WriteAsync($"{data.Values["message"] ?? "Uh oh!"}"))
+    .MapGet("{greeting}/{name}", (req, res, data) =>
+        res.WriteAsync($"{data.Values["greeting"]}, {data.Values["name"]}!"))
+    .MapGet("", (req, res, data) => res.WriteAsync("Hello, World!"))))
+{
+    Console.WriteLine("Use Ctrl-C to shut down the host...");
+    host.WaitForShutdown();
+}
+// A web application run on port:5000
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.UseRouting();
+app.MapGet("/", () => "It works");
+app.MapGet("/Vanh", () => greeting);
+app.Run();
